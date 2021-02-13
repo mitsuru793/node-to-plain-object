5aa68954b79a780412c13aae4902c7f008961012
mitsuru793 <mitsuru793@gmail.com>
2021-02-14 08:12:55 +0900

    refactor: extract type ToPlainObjectCallback

--- src/toPlainObject.ts
+++ src/toPlainObject.ts
@@ -5,25 +5,45 @@ type Plain = Primitive | Plain[] | PlainObject
 
 type ObjectRecord = Record<string, unknown>
 
+type ToPlainObjectCallback = (
+  plain: PlainObject,
+  key: string,
+  val: unknown
+) => PlainObject
+
+const toPlainObjectCallback: ToPlainObjectCallback = (
+  plain: PlainObject,
+  key: string,
+  val: unknown
+) => {
+  plain[key] = toPlainObject(val)
+  return plain
+}
+
 function arrayLikeToPlain(from: ArrayLike<unknown>): Plain[] {
   // Set/Map is not array like
   return Array.from(from).map((v) => toPlainObject(v))
 }
 
-function objectToPlainObject(from: ObjectRecord): PlainObject {
+function objectToPlainObject(
+  from: ObjectRecord,
+  callback: ToPlainObjectCallback = toPlainObjectCallback
+): PlainObject {
   return Object.entries(from).reduce((plain: PlainObject, [key, val]) => {
-    plain[key] = toPlainObject(val)
-    return plain
+    return callback(plain, key, val)
   }, {})
 }
 
-function mapToPlainObject(from: Map<unknown, unknown>): PlainObject {
-  const plain: PlainObject = {}
+function mapToPlainObject(
+  from: Map<unknown, unknown>,
+  callback: ToPlainObjectCallback = toPlainObjectCallback
+): PlainObject {
+  let plain: PlainObject = {}
   from.forEach((val, key) => {
     if (!is.string(key)) {
       throw new Error(`Map key must be string, but ${key}`)
     }
-    plain[key] = val
+    plain = callback(plain, key, val)
   })
   return plain
 }
